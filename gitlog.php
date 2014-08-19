0e3662c8a933bec7e0e272c21e9d0819038b8c4e
jamshad.ahmad@coeus-solutions.de <jamshad.ahmad@coeus-solutions.de>
2014-08-19 17:03:44 +0500

    better coding style in final version 3

--- gitlog.php
+++ gitlog.php
@@ -4,17 +4,19 @@ namespace Git_Log_Parser;
 /**
  * Driver code for Git-Log-Parser class.
  * 
- * This is driver code.This code here will trigger Insights function of
- * GitLogParser class.
+ * Triggers showInsights method of GitLogParser class
  * 
  */
-$arg =null;
+
+$recievedArgument =null;
+
 if($argc > 1)
 {
-    $arg=$argv[1];
+    $recievedArgument=$argv[1];
 }
+
 include 'gitlogparser.php';
-$obj=new GitLogParser();
-$obj->showInsights($arg);
-?>
+
+$parser=new GitLogParser();
+$parser->showInsights($recievedArgument);
 
--- gitlogparser.php
+++ gitlogparser.php
@@ -4,13 +4,13 @@ namespace Git_Log_Parser;
 /**
  * Git_Log_Parser can be used to extract extra info from git log.
  * 
- * This script can be used to parse Git Log and get some useful info out of it.
- * It takes Git Log and extracts information about contributors and also 
+ * Parses Git Log and get some useful info out of it.
+ * Takes Git Log and extracts information about contributors and also 
  * shows git log in  better format
  * 
  * @package Git-Log-Parser
  * @author  Jamshad Ahmad
- * @version 2.0
+ * @version 3.0
  * 
  */
 class GitLogParser
@@ -23,7 +23,7 @@ class GitLogParser
     /**
      * Shows info about contributors
      * 
-     * This is the function which gets git log and parse it into some usefull insights.It uses 
+     * Gets git log and parse it into some usefull insights.It uses 
      * two other helper functions.
      * 
      * @access public
@@ -31,15 +31,14 @@ class GitLogParser
      */
     public function showInsights($arg)
     {
-        $force_show = false;
-        $path_provided = false;
-        if($arg!=null)
-        {
-            if($arg=="--help")
-            {
+        $forceShow = false;
+        $isPathProvided = false;
+        
+        if($arg!=null){
+            if($arg=="--help"){
                 echo "You reached the help section of gitlog";
                 echo "\n\n";
-                echo "Running command without any argument will show Git Insights";
+                echo "Running command without any argument shows Git Insights";
                 echo "\n";
                 echo "then it will ask for an input to show Git Log";
                 echo "\n\n";
@@ -51,85 +50,104 @@ class GitLogParser
                 echo "\n\n";
 		echo "You can provide path infront of the command";
 		echo "\n";
-		echo "eg. :> php gitlog.php ~/path/to/your/repository";
+		echo "eg. :> php gitlog.php /path/to/your/repository";
 		echo "\n\n";
+                
                 return;
             }
-            else if($arg=="-s")
-            {
+            else if($arg=="-s"){
                 echo "Autoshow of Commit Log is Enabled";
                 echo "\n";
-                $force_show = true;
+                
+                $forceShow = true;
             }
-            else if(strpos($arg, '/') !== false)
-            {
-                $path_provided = true;
+            else if(strpos($arg, '/') !== false){
+                $isPathProvided = true;
+                
                 $this->dir = trim($arg);
             }
-            else
-            {
+            else{
                 echo "$arg is Not a valid command argument, try --help";
                 echo "\n";
+                
                 return;
             }
         }
-        if($path_provided==false)
-        {
+        
+        if($isPathProvided==false){
             echo "Please enter the path of your local repository:";
             echo "\n";
-            $this->dir = trim(fread(STDIN, 80));
+            
+            $this->dir = realpath(trim(fread(STDIN, 80)));
+            
+            while (!is_dir($this->dir)) {
+                echo "$this->dir is not a valid directory";
+                echo "\n";
+                echo "Please enter absolute path:";
+                echo "\n";
+                
+                $this->dir = realpath(trim(fread(STDIN, 80)));
+            }
         }
+        
         $output = array();
-        $U_Authors = array();
-        $T_Authors = array();
-        $commit_count = 0;
+        $uniqueAuthors = array();
+        $totalAuthors = array();
+        $commitCount = 0;
+        
         exec("cd $this->dir; git log ",$output);
-        foreach($output as $line)
-        {
-            if(strpos($line, 'Author')===0)
-            { //only if line starts from Author
-                $commit_count=$commit_count+1;
+        
+        foreach($output as $line){
+            if(strpos($line, 'Author')===0){ 
+                $commitCount++;
                 //Here starts parsing magic
-                if(!empty($line))
-                {
-                    $tmp = array();
-                    $tmp = explode(":", $line); //spliting
+                if(!empty($line)){
+                    $tmp = explode(":", $line);
                     $tmp2 = explode("<", $tmp[1]);
                     $tmp2[0]= trim($tmp2[0]);
-                    array_push($T_Authors, $tmp2[0]);
-                    if($this->isIn($U_Authors, $tmp2[0])===0)
-                    { //only if already not added
-                        array_push($U_Authors, $tmp2[0]); //Here tmp2[0] contains name of committer
+                    
+                    array_push($totalAuthors, $tmp2[0]); //tmp2[0] contains Author name
+                    
+                    if(!in_array($tmp2[0],$uniqueAuthors)){ //only if already not added
+                        array_push($uniqueAuthors, $tmp2[0]); 
                     }
                 }
             }
         }
-        echo "\nTotal commits by all users :  $commit_count";
-        echo "\n\n";
-        if($commit_count===0)
-        {
+        
+        if($commitCount===0){
+            echo "You don't have any commits yet";
+            echo "\n";
+            echo "Make sure that you're providing valid git directory";
+            echo "\n";
+            
             return;//No need to show contribution list
         }
+        
+        echo "\nTotal commits by all users :  $commitCount";
+        echo "\n\n";
+        
         echo "Here is the contributors list\n\n";
-        foreach ($U_Authors as $value)
-        {
+        
+        foreach ($uniqueAuthors as $value){
             printf("%-33s  ",$value);
             echo " || Commit Count: ";
-            printf("%3d",$this->occCount($T_Authors, $value));
+            printf("%3d",$this->occCount($totalAuthors, $value));
             echo "  ||  Contrib. : ";
-            printf("%02.2f",($this->occCount($T_Authors, $value))*100/($commit_count));
+            printf("%02.2f",($this->occCount($totalAuthors, $value))*100/($commitCount));
             echo "%\n";
         }
-        if($force_show==true)
-        {
-            $this->gitLog();
+        
+        if($forceShow==true){
+            $this->printGitLog();
         }
-        else
-        {
+        else{
             echo "\nEnter 'S' to show all commits or any other key to exit : ";
+            
             $c = fgetc(STDIN);
-            if($c==='s' || $c==='S'){
-                $this->gitLog();
+            
+            if(strtolower($c) === 's'){
+                $this->printGitLog();
             }
         }
     }
@@ -137,66 +155,40 @@ class GitLogParser
     /**
      * Shows git log in new format
      *
-     * This will dump git log saperated by commits and with new format. I found it on 
+     * Shows git log saperated by commits and with new format. Found it on 
      * github. Its original author is Ngo Minh Nam but now it is little edited
      *
      */
-    public function gitLog()
+    public function printGitLog()
     {
         $output=array();
-        exec("cd $this->dir; git log ",$output);
         $history = array();
-        foreach($output as $line)
-        {
-            if(strpos($line, 'commit')===0)
-            {
-                if(!empty($commit))
-                {
-                    array_push($history, $commit);	
+        
+        exec("cd $this->dir; git log ",$output);
+        
+        foreach($output as $line){
+            if(strpos($line, 'commit')===0){
+                if(!empty($commit)){
+                    array_push($history, $commit);
+                    
                     unset($commit);
                 }
+                
                 $commit['Hash']   = substr($line, strlen('commit'));
             }
-            else if(strpos($line, 'Author')===0)
-            {
+            else if(strpos($line, 'Author')===0){
                 $commit['Author'] = substr($line, strlen('Author:'));
             }
-            else if(strpos($line, 'Date')===0)
-            {
+            else if(strpos($line, 'Date')===0){
                 $commit['Date']   = substr($line, strlen('Date:'));
             }
-            else
-            {
-                if(strlen($line)>1)
-                {
-                    $commit['Message']  = $line;
-                }
+            else if(strlen($line)>1){
+                $commit['Message']  = $line;
             }
         }
+        
         print_r($history);
         echo "\n";
-    }
-    
-    /**
-     * Returns true if element is in the container
-     * 
-     * Searches a particular key from an array and returns 1 if it is present.
-     *
-     * @param  string recieves $array and a $str which is a key to search.
-     * 
-     * @return bool   returns either 1 or 0.
-     */
-    protected function isIn($array,$str)
-    {
-        
-        foreach($array as $line)
-        {
-            if($line==$str)
-            {
-                return 1;
-            }
-        }
-        return 0;
     }
     
     /**
@@ -211,13 +203,13 @@ class GitLogParser
     protected function occCount($array,$str)
     {
         $count=0;
-        foreach($array as $line)
-        {
-            if($line==$str)
-            {
+        
+        foreach($array as $line){
+            if($line==$str){
                 $count++;
             }
         }
+        
         return $count;
     }
 }
